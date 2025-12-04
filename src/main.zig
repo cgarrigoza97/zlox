0f9902cbc025ea912ed43e9fc111dc1f385a7af3
cgarrigoza97 <cgarrigoza@gmail.com>
2025-12-03 22:07:29 -0300

    test

--- src/main.zig
+++ src/main.zig
@@ -7,29 +7,65 @@ const VM = @import("vm.zig");
 
 pub fn main() !void {
     VM.init();
-    var chunk = Common.Chunk.init();
+    const args = std.os.argv;
 
-    var constant = chunk.addConstant(1.2);
-    chunk.write(@intFromEnum(Common.OpCode.op_constant), 123);
-    chunk.write(@intCast(constant), 123);
+    if (args.len == 1) {
+        try repl();
+    } else if (args.len == 2) {
+        // runFile(args[1]);
+    } else {
+        @panic("Usage: zlox [path]");
+    }
 
-    constant = chunk.addConstant(3.4);
-    chunk.write(@intFromEnum(Common.OpCode.op_constant), 123);
-    chunk.write(@intCast(constant), 123);
-
-    chunk.write(@intFromEnum(Common.OpCode.op_add), 123);
-
-    constant = chunk.addConstant(5.6);
-    chunk.write(@intFromEnum(Common.OpCode.op_constant), 123);
-    chunk.write(@intCast(constant), 123);
-
-    chunk.write(@intFromEnum(Common.OpCode.op_divide), 123);
-    chunk.write(@intFromEnum(Common.OpCode.op_negate), 123);
-
-    chunk.write(@intFromEnum(Common.OpCode.op_return), 123);
-
-    Debug.disassembleChunk(&chunk, "test chunk");
-    _ = VM.interpret(&chunk);
     VM.free();
-    chunk.free();
 }
+
+fn repl() !void {
+    var stdin_buffer: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
+
+    var line: [1024]u8 = undefined;
+    var w: std.io.Writer = .fixed(&line);
+
+    while (true) {
+        std.debug.print("> ", .{});
+
+        const lineLength = try stdin.streamDelimiter(&w, '\n');
+
+        const lineRead = line[0..lineLength];
+
+        std.debug.print("{s}", .{lineRead});
+        // VM.interpret(lineRead);
+    }
+}
+
+// fn runFile(path: [*:0]u8) void {
+//     const source = readFile(path);
+//     std.debug.print("{s}", .{source});
+//     // const result = VM.interpret(source);
+//     // std.debug.print("{s}", .{result});
+// }
+
+// fn readFile(path: [*:0]u8) !*const []u8 {
+//     var alloc = std.heap.c_allocator;
+//     defer _ = alloc.deinit();
+
+//     var file = try std.fs.cwd().openFile(path, .{ .mode = .read_only });
+//     defer file.close();
+
+//     var read_buf: [2]u8 = undefined;
+//     var file_reader: std.fs.File.Reader = file.reader(&read_buf);
+
+//     const reader = &file_reader.interface;
+
+//     var line = std.Io.Writer.Allocating.init(alloc);
+//     defer line.deinit();
+
+//     while (true) {
+//         _ = try reader.streamDelimiter(&line.writer, '\n');
+//         line.written();
+//     }
+
+//     return line;
+// }
